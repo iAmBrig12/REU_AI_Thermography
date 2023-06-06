fb92ada3824ac1e712dfcc910213b611e187f590
iAmBrig12 <Bthornock12@gmail.com>
2023-06-06 11:42:39 -0500

    stuff

--- Thermography_Dataset.py
+++ Thermography_Dataset.py
@@ -2,23 +2,32 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 class ThermDataset(Dataset):
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, direction=0, noise_scale=0, spec_scale=10**12):
 
         # read excel file and load row data into variables
         df = pd.read_excel(file_path)
-        x = df.iloc[:,:11].values
-        y = df.iloc[:,11:].values
+        temp = df.iloc[:,:11].values
+        spec = df.iloc[:,11:].values
 
-        sc = StandardScaler()
-        x_train = sc.fit_transform(x)
-        y_train = y
+        # scale spectrum data
+        spec = spec.multiply(spec_scale)
 
+        if noise_scale > 0:
+            if direction:
+                temp = np.random.normal(loc=0, scale=noise_scale, size=temp.size)
+            else:
+                spec = np.random.normal(loc=0, scale=noise_scale, size=spec.size)
 
-        self.X_train = torch.tensor(x_train, dtype=torch.float32)
-        self.y_train = torch.tensor(y_train)
+        if direction:
+            self.X_train = torch.tensor(temp, dtype=torch.float32)
+            self.y_train = torch.tensor(spec)
+        else:
+            self.X_train = torch.tensor(spec, dtype=torch.float32)
+            self.y_train = torch.tensor(temp, dtype=torch.float32)
 
     def __len__(self):
         return len(self.y_train)
