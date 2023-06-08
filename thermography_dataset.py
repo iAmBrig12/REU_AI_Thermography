efdb721473b6a2208888875fad21d209f4acc18b
iAmBrig12 <Bthornock12@gmail.com>
2023-06-08 11:48:32 -0500

    worked more on dataset class

--- thermography_dataset.py
+++ thermography_dataset.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import torch
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 def get_data(file_path='combined_data.xlsx', direction=0, spec_scale=10**12, noise_scale=0.01):
     df = pd.read_excel(file_path)
     temp = df.iloc[:,:11].values
     spec = df.iloc[:,11:].values
-
-    #spec = spec * spec_scale
 
     if noise_scale > 0:
         if direction:
@@ -38,32 +37,32 @@ def get_data(file_path='combined_data.xlsx', direction=0, spec_scale=10**12, noi
 class ThermDataset(Dataset):
 
     def __init__(self, fp, direction, noise_scale):
-        xy = pd.read_excel(fp)
-        temp = xy.iloc[:,:11].values
-        spec = xy.iloc[:,11:].values
+        self.df = pd.read_excel(fp)
+        self.temp = self.df.iloc[:,:11].values
+        self.spec = self.df.iloc[:,11:].values
 
-        if noise_scale > 0:
+        if noise_scale:
             if direction:
-                mean = temp.mean()
-                noise = np.random.normal(0, mean*noise_scale, temp.shape)
-                temp = temp + noise
+                mean = self.temp.mean()
+                noise = np.random.normal(0, mean*noise_scale, self.temp.shape)
+                self.temp = self.temp + noise
             else:
-                mean = spec.mean()
-                noise = np.random.normal(0, mean*noise_scale, spec.shape)
-                spec = spec + noise
+                mean = self.spec.mean()
+                noise = np.random.normal(0, mean*noise_scale, self.spec.shape)
+                self.spec = self.spec + noise
 
         scaler = StandardScaler()
         
         if direction:
-            temp = scaler.fit_transform(temp)
-            self.x = torch.tensor(temp, dtype=torch.float32)
-            self.y = torch.tensor(spec, dtype=torch.float32)
+            self.temp = scaler.fit_transform(self.temp)
+            self.x = torch.tensor(self.temp, dtype=torch.float32)
+            self.y = torch.tensor(self.spec, dtype=torch.float32)
         else:
-            spec = scaler.fit_transform(spec)
-            self.x = torch.tensor(spec, dtype=torch.float32)
-            self.y = torch.tensor(temp, dtype=torch.float32)
+            self.spec = scaler.fit_transform(self.spec)
+            self.x = torch.tensor(self.spec, dtype=torch.float32)
+            self.y = torch.tensor(self.temp, dtype=torch.float32)
 
-        self.n_samples = xy.shape[0]
+        self.n_samples = self.df.shape[0]
 
     def __getitem__(self, index):
         return self.x[index], self.y[index]
