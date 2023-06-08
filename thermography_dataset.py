a6a9358221e590b292e284d5f0becfd84e141432
iAmBrig12 <Bthornock12@gmail.com>
2023-06-08 14:33:55 -0500

    dataloader stuff

--- thermography_dataset.py
+++ thermography_dataset.py
@@ -3,36 +3,6 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
-def get_data(file_path='combined_data.xlsx', direction=0, spec_scale=10**12, noise_scale=0.01):
-    df = pd.read_excel(file_path)
-    temp = df.iloc[:,:11].values
-    spec = df.iloc[:,11:].values
-
-    if noise_scale > 0:
-        if direction:
-            mean = temp.mean()
-            noise = np.random.normal(0, mean*noise_scale, temp.shape)
-            temp = temp + noise
-        else:
-            mean = spec.mean()
-            noise = np.random.normal(0, mean*noise_scale, spec.shape)
-            spec = spec + noise
-
-    scaler = StandardScaler()
-    
-    if direction:
-        X = scaler.fit_transform(temp)
-        y = spec
-    else:
-        X = scaler.fit_transform(spec)
-        y = temp
-
-    X = torch.tensor(X, dtype=torch.float32)
-    y = torch.tensor(y, dtype=torch.float32)
-
-    return X, y
 
 class ThermDataset(Dataset):
 
