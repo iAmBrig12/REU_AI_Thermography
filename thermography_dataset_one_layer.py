8e59e9e3e1e438301ce071b2596172ab509304ce
iAmBrig12 <Bthornock12@gmail.com>
2023-06-14 09:33:19 -0500

    one layer prediction

--- thermography_dataset_one_layer.py
+++ thermography_dataset_one_layer.py
@@ -6,31 +6,26 @@ from sklearn.preprocessing import StandardScaler
 
 class ThermDataset(Dataset):
 
-    def __init__(self, fp, noise_scale, tandem=1, direction=0, spec_scale=10**12):
+    def __init__(self, fp, noise_scale, spec_scale=10**12):
         self.df = pd.read_excel(fp)
         self.wavelengths = self.df.columns[11:]
         temp = self.df.iloc[:,10].values
         spec = self.df.iloc[:,11:].values
 
-        if noise_scale:
-            if direction:
-                mean = temp.mean()
-                noise = np.random.normal(0, mean*noise_scale, temp.shape)
-                self.temp = temp + noise
-            else:
-                mean = spec.mean()
-                noise = np.random.normal(0, mean*noise_scale, spec.shape)
-                spec = spec + noise
+        
+        mean = spec.mean()
+        noise = np.random.normal(0, mean*noise_scale, spec.shape)
+        spec = spec + noise
 
         spec = spec * spec_scale
-        self.x = torch.tensor(temp, dtype=torch.float32)
-        self.y = torch.tensor(spec, dtype=torch.float32)
+        self.temp = torch.tensor(temp, dtype=torch.float32).reshape(-1, 1)
+        self.spec = torch.tensor(spec, dtype=torch.float32)
         
 
         self.n_samples = self.df.shape[0]
 
     def __getitem__(self, index):
-        return self.x[index], self.y[index]
+        return self.temp[index], self.spec[index]
 
     def __len__(self):
         return self.n_samples
